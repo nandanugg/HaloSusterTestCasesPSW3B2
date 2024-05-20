3c4b1c9c82cc8e46c3790ab0511c77944ae8d624
nandanugg <nandanugg@gmail.com>
2024-05-20 18:41:03 +0700

    fix

--- srv/service/nurseNip.go
+++ srv/service/nurseNip.go
@@ -8,7 +8,7 @@ func (c *NipService) IncrementNurseNipUsedIndex() {
 
 func (c *NipService) GetNurseNip() uint64 {
 	c.nurseIndexMutex.Lock()
-	defer c.IncrementItNipUsedIndex()
+	defer c.IncrementNurseNipUsedIndex()
 	defer c.nurseIndexMutex.Unlock()
 	return c.nurseNIPs[c.nurseUsedIndexNIP]
 }
